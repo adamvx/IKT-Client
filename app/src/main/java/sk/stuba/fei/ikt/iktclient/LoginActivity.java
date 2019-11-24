f8fa8401351c9cefa5f8eb8e57b61be9b32847f1
Milan Ponist <milan_ponist@hotmail.com>
2019-11-24 12:19:05 +0100

    Dependency update

--- app/src/main/java/sk/stuba/fei/ikt/iktclient/LoginActivity.java
+++ app/src/main/java/sk/stuba/fei/ikt/iktclient/LoginActivity.java
@@ -3,12 +3,33 @@ package sk.stuba.fei.ikt.iktclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn = (Button)findViewById(R.id.LoginBtn);
+    EditText fillPassword = (EditText)findViewById(R.id.FillPassword);
+    EditText fillName = (EditText)findViewById(R.id.FillName);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                if(fillName.getText().toString().equals("admin") && fillPassword.getText().toString().equals("admin")) {
+                    // launch activity
+                    Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT);
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Incorrect name or password", Toast.LENGTH_SHORT);
+                }
+            }
+            });
     }
 }
--- app/src/main/java/sk/stuba/fei/ikt/iktclient/MainActivity.java
+++ app/src/main/java/sk/stuba/fei/ikt/iktclient/MainActivity.java
@@ -1,6 +1,8 @@
 package sk.stuba.fei.ikt.iktclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
